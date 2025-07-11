27bc46ab1f86bdb6e6093a20e6d94986ebd82381
Nitin1solutions <nitin@1solutions.biz>
2025-07-11 11:10:44 +0530

    fixed mobile responsive bug

--- src/components/RegistrationClosed.tsx
+++ src/components/RegistrationClosed.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const RegistrationClosed: React.FC = () => {
   return (
-    <section className="bg-black p-40">
-    <div className="bg-black font-orbitron text-neon-blue rounded-lg text-center text-6xl font-bold shadow-lg  mx-auto">
-      Registration Closed
-    </div>
-    </section>
+ <section className="bg-black p-10 md:p-40">
+  <div className="bg-black font-orbitron text-neon-blue rounded-lg text-center text-4xl md:text-6xl font-bold shadow-lg mx-auto">
+    Registration Closed
+  </div>
+</section>
   );
 };
 
