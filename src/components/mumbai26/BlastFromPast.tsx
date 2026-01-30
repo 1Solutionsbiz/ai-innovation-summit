446560452f92b8f3a179100fe0a2e166e5af930d
Dolphin <pythondolphin@gmail.com>
2026-01-30 17:48:06 +0530

    homepage new video

--- src/components/mumbai26/BlastFromPast.tsx
+++ src/components/mumbai26/BlastFromPast.tsx
@@ -1,5 +1,5 @@
 const BlastFromPast = () => {
-  const videoSrc = "videos/Yt_promo_10_sept.mp4";
+  const videoSrc = "videos/GUILD-Highlight-26.mp4";
   const videoTitle = "AI Innovation Summit 2024 Highlights";
 
   return (
