260c7cfeb6005a50d6823cac992c3729b6125d5e
Dolphin <pythondolphin@gmail.com>
2026-09-18 15:00:18 +0530

    mobile menu

--- src/components/gurugram_final/RedesignNavigation.tsx
+++ src/components/gurugram_final/RedesignNavigation.tsx
@@ -123,7 +123,7 @@ const RedesignNavigation = () => {
       </div>
 
       {open && (
-        <div className="mobile-redesign-menu mobile-redesign-menu-open">
+        <div className="mobile-redesign-menu mobile-redesign-menu-open gap-1">
           {links.map((link) => (
             <a
               key={link.label}
